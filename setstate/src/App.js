ac773134e128068e9d933fa436cfb769778f7ffb
Gokul <gokulramalingam1@gmail.com>
2019-10-29 22:26:12 +0530

    Update in App component

--- setstate/src/App.js
+++ setstate/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './component/Counter'
+import Counter1 from './component/Counter1'
+import Counter2 from './component/Counter2'
+import Counter3 from './component/Counter3'
 function App() {
   return (
     <div className="App">
     <Counter/>
+    <Counter1/>
+    <Counter2/>
+    <Counter3/>
     </div>
   );
 }
