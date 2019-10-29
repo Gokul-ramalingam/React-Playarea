faf76892490b81bf168b976e702ea6155cc8b038
Gokul <gokulramalingam1@gmail.com>
2019-10-29 19:21:30 +0530

    Implementation of state

---
+++ state/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Message from './component/Message';
+
+function App() {
+  return (
+    <div className="App">
+      <Message/>
+    </div>
+  );
+}
+
+export default App;
